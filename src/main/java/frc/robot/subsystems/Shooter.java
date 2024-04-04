5f55a3fe87170876fde3230421f26a2cfcfe12c8
joyadomari-chun24 <joyadomari-chun24@punahou.edu>
2024-04-03 19:32:50 -1000

    Made velocity checker accurate to rpm
    ...I think.

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -33,8 +33,8 @@ public class Shooter extends SubsystemBase {
     @Override
     public void periodic() {
         //getSelectedSensorVelocity returns ticks per 100ms, so we can just convert to rpm w/ dimensional analysis
-        velocityL = shooterControllerL.getSelectedSensorVelocity() / 4096 * 1000 * 60;
-        velocityR = shooterControllerR.getSelectedSensorVelocity() / 4096 * 1000 * 60;
+        velocityL = shooterControllerL.getSelectedSensorVelocity() / 4096 / 10 * 60;
+        velocityR = shooterControllerR.getSelectedSensorVelocity() / 4096 / 10 * 60;
         SmartDashboard.putNumber("Shooter Velocity (Left)", velocityL);
         SmartDashboard.putNumber("Shooter Velocity (Right)", velocityR);
     }
