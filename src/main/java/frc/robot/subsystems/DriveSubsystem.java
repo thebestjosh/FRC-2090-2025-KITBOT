35b937599123b0c4459a46ea41c22c3b542d37d7
thebestjosh <156040143+thebestjosh@users.noreply.github.com>
2025-02-18 16:05:27 -1000

    Update DriveSubsystem.java

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,17 +1,25 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveSubsystem extends SubsystemBase {
-    private final PWMSparkMax leftMotor = new PWMSparkMax(0);  // Change ports as needed
-    private final PWMSparkMax rightMotor = new PWMSparkMax(1); // Change ports as needed
+    private final PWMSparkMax leftMotor1 = new PWMSparkMax(15);
+    private final PWMSparkMax leftMotor2 = new PWMSparkMax(14);
+    private final PWMSparkMax rightMotor1 = new PWMSparkMax(13);
+    private final PWMSparkMax rightMotor2 = new PWMSparkMax(12);
 
-    private final DifferentialDrive drive = new DifferentialDrive(leftMotor, rightMotor);
+    private final MotorControllerGroup leftMotors = new MotorControllerGroup(leftMotor1, leftMotor2);
+    private final MotorControllerGroup rightMotors = new MotorControllerGroup(rightMotor1, rightMotor2);
+
+    private final DifferentialDrive drive = new DifferentialDrive(leftMotors, rightMotors);
 
     public DriveSubsystem() {
-        rightMotor.setInverted(true); // Ensures correct direction
+        // Adjust motor inversion based on testing
+        leftMotors.setInverted(true);  // If motors run in opposite directions, invert as needed
+        rightMotors.setInverted(false);
     }
 
     public void arcadeDrive(double forward, double rotation) {
